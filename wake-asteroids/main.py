322b2d1094c199bbc89cf8e8d3f14e65aefaba30
Wakethelight <chris.degner@gmail.com>
2025-08-22 13:00:07 -0400

    updated main.py

--- wake-asteroids/main.py
+++ wake-asteroids/main.py
@@ -8,13 +8,6 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
 
     while True:
         for event in pygame.event.get():
