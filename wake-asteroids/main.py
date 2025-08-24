7286e36e5294787d1a3b9e61ebe56c09cd035961
Wakethelight <chris.degner@gmail.com>
2025-08-24 14:37:58 -0400

    updated to do a and d movement

--- wake-asteroids/main.py
+++ wake-asteroids/main.py
@@ -3,7 +3,6 @@
 # throughout this file
 import pygame
 from constants import *
-from circleshape import CircleShape
 from player import Player
 
 
@@ -11,17 +10,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
     dt = 0
-    Player(x = SCREEN_WIDTH // 2, y = SCREEN_HEIGHT // 2)
 
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        Player.draw(screen)
-        screen.fill("black") 
+        
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        player.update(dt)
 
         dt = clock.tick(60) / 1000  # Limit to 60 frames per second
 
--- wake-asteroids/player.py
+++ wake-asteroids/player.py
@@ -1,11 +1,11 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        rotation = 0
+        self.rotation = 0
     
     # in the player class
     def triangle(self):
@@ -16,5 +16,17 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
-    def draw(screen):
+    def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt 
+    
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
