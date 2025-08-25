e58bded5e0895ac27485715667471815995c932c
Wakethelight <chris.degner@gmail.com>
2025-08-24 23:35:17 -0400

    moving asteroids game over

--- wake-asteroids/main.py
+++ wake-asteroids/main.py
@@ -1,31 +1,62 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
+    #initialize pygame
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
     
+    # create a window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # create a clock object to manage the frame rate
+    clock = pygame.time.Clock()
+
+    # groups to hold game objects
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    #create the object in the groups
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+    # create the player object
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    asteroid_field = AsteroidField()
 
     dt = 0
 
-
+    # main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        #
+        updateable.update(dt)
         
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-        player.update(dt)
+        for obj in asteroids:
+            if obj.collide(player):
+                print("Game over!")
+                sys.exit()
 
+        # set screen to black
+        screen.fill("black")
+
+        # draw all objects
+        for object in drawable:
+            object.draw(screen)
+        #refresh the display
+        pygame.display.flip()
+        
+        # manage frame rate
         dt = clock.tick(60) / 1000  # Limit to 60 frames per second
 
 
